97d973a9c7e628cb0e63dedf103a0a13fed25e70
NetherDonMC <mr.do0ob@mail.ru>
2025-06-11 11:21:20 +0300

    Change weights

--- common/src/main/java/ru/netherdon/netheragriculture/registries/NAPiglinBartering.java
+++ common/src/main/java/ru/netherdon/netheragriculture/registries/NAPiglinBartering.java
@@ -14,27 +14,27 @@ public class NAPiglinBartering
         return List.of(
             LootItem.lootTableItem(NAItems.CRIMSON_BERRY_SEEDS.value())
                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(3, 8)))
-                .setWeight(35)
+                .setWeight(40)
                 .build(),
 
             LootItem.lootTableItem(NAItems.CRIMSON_BERRY.value())
-                .apply(SetItemCountFunction.setCount(UniformGenerator.between(5, 10)))
-                .setWeight(25)
+                .apply(SetItemCountFunction.setCount(UniformGenerator.between(3, 10)))
+                .setWeight(30)
                 .build(),
 
             LootItem.lootTableItem(NAItems.SINFUL_EYES.value())
                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(3, 8)))
-                .setWeight(35)
+                .setWeight(40)
                 .build(),
 
             LootItem.lootTableItem(NAItems.MORTOFRUCT_HALF.value())
                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(2, 9)))
-                .setWeight(35)
+                .setWeight(40)
                 .build(),
 
             LootItem.lootTableItem(NAItems.DEAD_VINES.value())
                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 5)))
-                .setWeight(30)
+                .setWeight(35)
                 .build()
         );
     }
